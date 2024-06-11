6b2d1ac916f552b50ac2b60b91a2f071a1641d72
satrong <satrong@foxmail.com>
2024-06-11 11:37:32 +0800

    feat: add layout and menu

---
+++ src/layouts/TheMain.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import TheMenu from '~/layouts/TheMenu.vue'
+
+const appName = import.meta.env.VITE_APP_NAME
+</script>
+
+<template>
+  <ElContainer>
+    <ElMain>
+      <aside class="fixed inset-0 w-[var(--co-aside-width)] flex justify-end bg-[var(--el-bg-color-page)]">
+        <div class="h-full w-[var(--co-aside-content-width)] overflow-y-auto px-2">
+          <div class="flex items-center border-bottom py-3">
+            <CoLogo size="2.5rem" />
+            <div class="flex-grow-1">
+              <div class="text-xs ml-2 font-mono">
+                {{ appName }}
+              </div>
+              <div class="flex justify-end">
+                <TheMenu class="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </aside>
+      <main class="layout-main">
+        <RouterView />
+      </main>
+    </ElMain>
+  </ElContainer>
+</template>
---
+++ src/layouts/TheMenu.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="flex items-center">
+    <ElTooltip :content="t('menu.colorMode')">
+      <CoColorMode class="mx-2" />
+    </ElTooltip>
+    <ElTooltip :content="t('menu.instructions')">
+      <RouterLink to="/instructions" active-class="active">
+        <Icon icon="heroicons:command-line" />
+      </RouterLink>
+    </ElTooltip>
+    <ElTooltip :content="t('menu.apiConfig')">
+      <RouterLink to="/apiConfig" active-class="active">
+        <Icon icon="hugeicons:api" />
+      </RouterLink>
+    </ElTooltip>
+    <ElTooltip :content="t('menu.settings')">
+      <RouterLink to="/settings" active-class="active">
+        <Icon icon="material-symbols:settings" />
+      </RouterLink>
+    </ElTooltip>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:deep() a {
+  display: inline-flex;
+  align-items: center;
+  color: var(--el-text-color-secondary);
+  margin: 0 0.5rem;
+  font-size: 1.5rem;
+  line-height: 100%;
+  &:hover, &.active {
+    color: var(--el-color-primary);
+  }
+}
+</style>
